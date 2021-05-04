257da13d85bd642764c76d4c4e67685a1d394f92
ArtyomArtamonov <artem02artamonov@gmail.com>
2021-05-04 16:00:08 +0300

    lessons description when get-info

--- OpenMind/Domain/Courses/BriefCourseLessonResult.cs
+++ OpenMind/Domain/Courses/BriefCourseLessonResult.cs
@@ -3,6 +3,7 @@ namespace OpenMind.Domain.Courses
     public class BriefCourseLessonResult
     {
         public string Title { get; set; }
+        public string Description { get; set; }
         public int LessonNumber { get; set; }
     }
 }
